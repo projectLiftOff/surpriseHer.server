5180e1e85594acc5248ee2ea50291041c077f1c9
Armando Perez <armandopmj@gmail.com>
2015-10-18 16:14:28 -0700

    fix(localVars): update file name of environment variables

--- server/config/environment.setup.js
+++ server/config/environment.setup.js
@@ -6,7 +6,7 @@ function setupEnvironmentVariables() {
     } else {
         console.log('runnning in dev mode');
         var fs = require('fs');
-        var environmentVariables = JSON.parse(fs.readFileSync('local_environment_variables.json', 'utf-8'));
+        var environmentVariables = JSON.parse(fs.readFileSync('environmentVariables.json', 'utf-8'));
         for (var key in environmentVariables) {
             process.env[key] = environmentVariables[key];
         }

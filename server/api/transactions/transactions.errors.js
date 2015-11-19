cffe596d03bdfed22f22a16e2ee925d6818c21e6
Mike Luby <mLuby@users.noreply.github.com>
2015-11-18 23:06:43 -0800

    fix `no address error` message

--- server/api/transactions/transactions.errors.js
+++ server/api/transactions/transactions.errors.js
@@ -4,7 +4,7 @@ module.exports = {
   wrongNumberArgumentsIncompleteUser: "Please only send the following info: date and gift. In the following format: \n \n date gift",
   invalidDate: "You have provided an invalid date. Please correct the error and text us back!",
   invalidGiftName: "You have provided an invalid gift. Please correct the error and text us back!",
-  invalidAddressCodeName: "You have provided an invalid gift. Please correct the error and text us back!",
+  invalidAddressCodeName: "You have provided an invalid address code. Please correct the error and text us back!",
   paymentFaild: "Hmm.. Seems like there was a problem charging your card... please contact customer support: adfkjdf@ask.com or 444444444 ",
   giftNotAvalible: "Hmm... the gift you ordered is not avalible this month, please order one of the gifts provided in this months list",
   // secondTransaction: "You have already ordered a gift this month... If you want to change your order or order an additional gift please contact customer support",

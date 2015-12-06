f7f8e997f4198fa15bf67f7390634daed965033b
Armando Perez <armandopmj@gmail.com>
2015-12-05 22:13:58 -0800

    update(errorMessages): update copy

--- server/api/errorsMessages.js
+++ server/api/errorsMessages.js
@@ -1,22 +1,20 @@
 module.exports = {
   // C: Transactions Contoller
-  phoneNumberNotFound: "Hmm... we don't seem to recognize this number... to register for awesome gifts! go to surpriseher.co",
-  wrongNumberArgumentsCompleteUser: "Please only send the following info: date, gift, address. In the following format: \n \n date gift address",
-  wrongNumberArgumentsIncompleteUser: "Please only send the following info: date and gift. In the following format: \n \n date gift",
-  invalidDate: "You have provided an invalid date. Please correct the error and text us back!",
-  invalidGiftName: "You have provided an invalid gift. Please correct the error and text us back!",
-  invalidAddressCodeName: "You have provided an invalid address code. Please correct the error and text us back!",
-  paymentFaild: "Hmm.. Seems like there was a problem charging your card... please contact us support at hello@surpriseher.co or 415-985-4438",
-  giftNotAvalible: "Hmm... the gift you ordered is not avalible this month, please order one of the gifts provided in this months list",
-  // secondTransaction: "You have already ordered a gift this month... If you want to change your order or order an additional gift please contact customer support",
-  missedOrderWindow: "Sorry but you missed the order window... but don't worry we send out a new gift list on the 25th of each month!",
-  generic: "oops.. looks like there was an error please try again or contact customer support at hello@surpriseher.co or 415-985-4438",
-
+  phoneNumberNotFound: "Hmm... we don't seem to recognize this number. Register at surpriseher.co to order surprise gifts she'll love!",
+  wrongNumberArgumentsCompleteUser: "Please only send the day, gift, and address code, using the following format: \n \n '12 necklace herwork'",
+  wrongNumberArgumentsIncompleteUser: "Please only send the day and gift, using the following format: \n \n '12 necklace'",
+  invalidDate: "You have provided an invalid date. Please re-submit your text with a valid day (1-31).",
+  invalidGiftName: "You have provided an invalid gift name. Please re-submit your text using one of the gift names listed in our original message.",
+  invalidAddressCodeName: "You have provided an invalid address code. Please re-submit your text using one of your address codes listed in our original message.",
+  paymentFaild: "Hmm... seems like there was a problem charging your card. Let's get this resolved! Please contact customer support at hello@surpriseher.co or 415-985-4438",
+  giftNotAvalible: "Hmm... the gift you ordered is not available this month. Please re-submit your text using one of the gifts available in this month's list.",
+  // secondTransaction: "You have already ordered a gift this month. If you want to change your order or place an additional order, please contact customer support at hello@surpriseher.co or 415-985-4438",
+  missedOrderWindow: "Oh no, you missed the ordering window this month! Not to worry, we send out a new gift list on the 25th. Place your order by the 28th and she'll be smiling in no time!",
+  generic: "Oops... there was an error on our end. Let's get this resolved! Please try again or contact customer support at hello@surpriseher.co or 415-985-4438",
   // C: Users Controller
-  contactSupportAddresses: "Hmm... we had trouble adding your addresses into our system please contact us support at hello@surpriseher.co or 415-985-4438",
-  contactSupportCreateBraintree: "Hmm... we had trouble adding your card information into our system please contact us support at hello@surpriseher.co or 415-985-4438",
-  contactSupportTxtMessenger: "You successfully registered!  However, we had trouble sending you a txt with our system please contact us support at hello@surpriseher.co or 415-985-4438 seem",
-
+  contactSupportAddresses: "Hmm... we had trouble adding your addresses to our system. Let's get this resolved! Please contact customer support at hello@surpriseher.co or 415-985-4438",
+  contactSupportCreateBraintree: "Hmm... we had trouble adding your card information to our system. Let's get this resolved! Please contact customer support at hello@surpriseher.co or 415-985-4438",
+  contactSupportTxtMessenger: "You successfully registered!  However, we had trouble sending you a txt with our system. Let's get this resolved! Please contact customer support at hello@surpriseher.co or 415-985-4438",
   // C: General
-  contactSupport: "There seems to be a system error please contact us support at hello@surpriseher.co or 415-985-4438"
+  contactSupport: "Oops... there was an error on our end. Let's get this resolved! Please try again or contact customer support at hello@surpriseher.co or 415-985-4438"
 }

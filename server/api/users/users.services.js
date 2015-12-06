607517c2e98c517664c96ebea459528977d7176b
Armando Perez <armandopmj@gmail.com>
2015-12-06 13:57:31 -0800

    update(userCopy): update txt messges upon user signup and fix grammer errors

--- server/api/users/users.services.js
+++ server/api/users/users.services.js
@@ -13,18 +13,18 @@ function constructAddressCodesOptionsStr (addressCodes) {
 }
 
 function constructSignUpGiftOptionsMessages (userRegistered, gifts, addresses) {
-  let message = ""
-  if (userRegistered) {
-    const addressCodes = addresses.map(address => address.code_name)
-    message = `Hey! You have successfully signed up for SurpriseHer monthly limited gift lists! Here is our curated gift selection only avalible to new signups:
-    \n${constructGiftOptionsStr(gifts)}PRODUCT URL
-    \nTo begin the order process, txt back the gift CODE, the day in month you'd like us to ship and the shipping address CODE (Your address CODEs are below) you want us to send the gift to (e.g 3 ${gifts[0].look_up} ${addressCodes[0]}, 18 ${gifts[1].look_up} ${addressCodes[0]})
-    \n${constructAddressCodesOptionsStr(addressCodes)}`
-  } else {
-    message = `Hey! You have successfully signed up for SurpriseHer monthly limited gift lists! Here is our curated gift selection only avalible to new signups:
-    \n${constructGiftOptionsStr(gifts)}PRODUCT URL
-    \nTo begin the order process, txt back the gift CODE and the day in month you'd like us to ship (e.g 3 ${gifts[0].look_up}, 18 ${gifts[1].look_up})`
-  }
+  let message = 'Hi, this is SurpriseHer, we’re excited you signed up! SurpriseHer: a simple way to keep the spark alive and remind her she’s special. You will receive a text early January with our first-ever gift list. You can start surprising her as early as next month!'
+  // if (userRegistered) {
+  //   const addressCodes = addresses.map(address => address.code_name)
+  //   message = `Hey! You have successfully signed up for SurpriseHer monthly limited gift lists! Here is our curated gift selection only avalible to new signups:
+  //   \n${constructGiftOptionsStr(gifts)}PRODUCT URL
+  //   \nTo begin the order process, txt back the gift CODE, the day in month you'd like us to ship and the shipping address CODE (Your address CODEs are below) you want us to send the gift to (e.g 3 ${gifts[0].look_up} ${addressCodes[0]}, 18 ${gifts[1].look_up} ${addressCodes[0]})
+  //   \n${constructAddressCodesOptionsStr(addressCodes)}`
+  // } else {
+  //   message = `Hey! You have successfully signed up for SurpriseHer monthly limited gift lists! Here is our curated gift selection only avalible to new signups:
+  //   \n${constructGiftOptionsStr(gifts)}PRODUCT URL
+  //   \nTo begin the order process, txt back the gift CODE and the day in month you'd like us to ship (e.g 3 ${gifts[0].look_up}, 18 ${gifts[1].look_up})`
+  // }
   return message
 }
 
